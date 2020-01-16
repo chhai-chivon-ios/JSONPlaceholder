ff280902f83128e91bdf920abb1872175e1f0c58
Chhai Chivon <chhaichivon1995@gmail.com>
2020-01-17 05:55:20 +0700

    Initial Commit

---
+++ JSONPlaceholder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONPlaceholder
+//
+//  Created by Chhai Chivon on 1/17/20.
+//  Copyright © 2020 Chhai Chivon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
